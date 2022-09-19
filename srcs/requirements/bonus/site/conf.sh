18f836f81c6851616e0ef21c5336fc3e5dc2ed06
Theodore NATON <tnaton@e1r10p8.clusters.42paris.fr>
2022-09-19 13:28:06 -0400

    site qui marche

--- srcs/requirements/bonus/site/conf.sh
+++ srcs/requirements/bonus/site/conf.sh
@@ -3,9 +3,10 @@
 
 mkdir -p /inception/site;
 
-if [ "test -e /inception/site/index.htlm" ]
+if [ ! -e /inception/site/templates/index.html ]
 then
-	cp /tmp/index.html /inception/site/index.html
+	mkdir -p /inception/site/templates;
+	cp /tmp/index.html /inception/site/templates/index.html
 fi
 
 chown -R www-data:www-data /inception/site
@@ -15,7 +16,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/inception/site/')
+@app.route('/')
 def site():
     return render_template(\"index.html\")
 
@@ -28,4 +29,4 @@ if __name__ == \"__main__\":
 
 echo "launching flask"
 
-exec gunicorn --chdir /inception/site --bind 0.0.0.0:5000 --name "site_statique" --timeout 60 --workers 3 wsgi:app
+exec gunicorn --chdir /inception/site --bind 0.0.0.0:5000 --name "site" --timeout 60 --workers 3 wsgi:app
